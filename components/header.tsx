e016ade97922dd7c258a7d4d214628eaeaaba32e
Van Nguyen <van.nguyenthi@asnet.com.vn>
2020-12-24 08:24:19 +0700

    Add practice with nextJS

---
+++ components/header.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Container from './container'
+
+export default function Header() {
+  return (
+    <Container>
+      <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+        <Link href="/">
+          <a className="hover:underline">Header</a>
+        </Link>
+      </h2>
+    </Container>
+  )
+}
---
+++ components/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from './footer'
+import Header from './header'
+import Meta from './meta'
+
+export default function Layout({ preview, children }) {
+  return (
+    <>
+      <Meta />
+      <Header />
+      <div className="min-h-screen">
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
+  )
+}
