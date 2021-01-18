9be46d931c674366898570282cf3a3e5a7ba615d
Van Nguyen <van.nguyenthi@asnet.com.vn>
2021-01-19 01:19:03 +0700

    Update param preview

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,7 +12,7 @@ import Tabs from '../../components/tab'
 import SliderNav from '../../components/product/slide-nav'
 import InfoProduct from '../../components/product/info'
 
-export default function Product({ allProducts }){
+export default function Product({ preview, allProducts }){
   const { query } = useRouter()
   const dataProduct = allProducts && allProducts.filter(({ node }) => node._meta.uid === query.id)
   const dataImagesProduct = dataProduct && dataProduct.map(({ node }) => node.images) 
@@ -33,24 +33,29 @@ export default function Product({ allProducts }){
   const numComments = productReviewComments && productReviewComments.length
 
    return (
-    <>      
-      {dataProductLength && 
-        <div className=''>
-          <Banner sex={sexType} navList={navListProduct}/>
-          <div className="flex flex-col m-auto -mt-20 items-center justify-between bg-white w-11/12 h-full border-solid border	sborder-gray-50">
-          <div className="flex w-full">
-            <InfoProduct product={dataProduct[0].node}/>
-            {imagesRelative.length > 0 &&
-              <SliderNav dataProduct={imagesRelative}/>
-            }
+    <>
+      <Layout preview={preview}>
+        <Head>
+          <title> Product Detail</title>
+        </Head>
+        {dataProductLength && 
+          <div className=''>
+            <Banner sex={sexType} navList={navListProduct}/>
+            <div className="flex flex-col m-auto -mt-20 items-center justify-between bg-white w-11/12 h-full border-solid border	sborder-gray-50">
+            <div className="flex w-full">
+              <InfoProduct product={dataProduct[0].node}/>
+              {imagesRelative.length > 0 &&
+                <SliderNav dataProduct={imagesRelative}/>
+              }
+            </div>
+              {numComments > 0 &&
+                <Tabs reviews={productReviewComments}/>
+              }
+              <ProductList allDatas={productTypePopular} loadMoreNumber={3}/>
+            </div>
           </div>
-            {numComments > 0 &&
-              <Tabs reviews={productReviewComments}/>
-            }
-            <ProductList allDatas={productTypePopular} loadMoreNumber={3}/>
-          </div>
-        </div>
-      }
+        }
+      </Layout>
     </>
   )
 }
